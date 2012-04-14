1114261b4e1051b33bd76d420d9bd41d428ad31d
Dylan Swiggett <omnipotentperson@gmail.com>
2012-04-14 10:52:22 -0700

    test

--- 24-hour-android/src/android/game/androiGameActivity.java
+++ 24-hour-android/src/android/game/androiGameActivity.java
@@ -9,5 +9,7 @@ public class androiGameActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        //Test
     }
 }
