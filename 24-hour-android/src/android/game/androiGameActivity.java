30926e4bf13b2f8987ce3dc571d15fd2a995b3d1
Dylan Swiggett <omnipotentperson@gmail.com>
2012-04-14 10:39:11 -0700

    Initial structure

---
+++ 24-hour-android/src/android/game/androiGameActivity.java
@@ -0,0 +1,13 @@
+package android.game;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class androiGameActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
