f03b33093e273d64a645609fcac24b40b25a477b
Dylan Swiggett <omnipotentperson@gmail.com>
2012-04-14 19:17:20 -0700

    Added a test level file, and level loading capability.

--- 24-hour-android/src/android/game/LibgdxTest.java
+++ 24-hour-android/src/android/game/LibgdxTest.java
@@ -1,12 +1,17 @@
 package android.game;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
+
+import android.util.Log;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Audio;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.input.GestureDetector;
@@ -47,19 +52,20 @@ public class LibgdxTest implements ApplicationListener{
         /*
          * Creates planets
          */
-        Random random = new Random();
-        for (int i = 0; i < 20; i++){
-        	int planetDecide = random.nextInt(100);
-        	String planet;
-        	if (planetDecide > 50){
-        		planet = "sun.png";
-        	} else if (planetDecide > 25){
-        		planet = "neptune.png";
-        	} else {
-        		planet = "mars.png";
-        	}
-        	planets.add(new Planet(planet, new Vector3D(random.nextInt(5000), random.nextInt(5000), 0), random.nextInt(500)));
-        }
+//        Random random = new Random();
+//        for (int i = 0; i < 20; i++){
+//        	int planetDecide = random.nextInt(100);
+//        	String planet;
+//        	if (planetDecide > 50){
+//        		planet = "sun.png";
+//        	} else if (planetDecide > 25){
+//        		planet = "neptune.png";
+//        	} else {
+//        		planet = "mars.png";
+//        	}
+//        	planets.add(new Planet(planet, new Vector3D(random.nextInt(5000), random.nextInt(5000), 0), random.nextInt(500)));
+//        }
+        loadLevel("testLvl.lvl");
         
         audio = Gdx.audio;
         bambi = audio.newMusic(Gdx.files.internal("bambi.ogg"));
@@ -115,6 +121,53 @@ public class LibgdxTest implements ApplicationListener{
 	}
 	
 	public void resume() {
+		
+	}
+	
+	/*
+	 * Load the level stored in the specified file.
+	 */
+	public void loadLevel(String levelName){
+		FileHandle file = Gdx.files.internal(levelName);
+		BufferedReader reader = new BufferedReader(file.reader());
+		
+		String line;
+		
+		planets = new ArrayList<Planet>();
+		
+		try {
+			while ((line = reader.readLine()) != null){
+				String[] elements = line.split(" ");
+				String filename;
+				Planet.type type;
+				int x, y, r, m;
+				
+				Log.v("LibgdxTest", line);
+				
+				if (elements[0].equals("win")){
+					type = Planet.type.WIN;
+					filename = "earth.png";
+				} else if (elements[0].equals("friendly")){
+					type = Planet.type.FRIENDLY;
+					filename = "neptune.png";
+				} else if (elements[0].equals("hostile")){
+					type = Planet.type.HOSTILE;
+					filename = "mars.png";
+				} else {
+					type = Planet.type.NEUTRAL;
+					filename = "sun.png";
+				}
+				
+				x = Integer.parseInt(elements[1]);
+				y = Integer.parseInt(elements[2]);
+				r = Integer.parseInt(elements[3]);
+				m = Integer.parseInt(elements[4]);
+				
+				planets.add(new Planet(filename, type, new Vector3D(x, y, 0), r, m));
+			}
+		} catch (IOException e) {
+			Log.e("LibgdxTest", "Failed to load level " + levelName);
+		}
 		
 	}
 
